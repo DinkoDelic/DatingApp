d4e2fc84a6b14bc69b0378bea75f47c0c1d154e0
Dinko Delic <delic.dinko94@gmail.com>
2020-07-13 15:10:51 +0100

    Added users list and detailed view UI

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,20 @@
+import { Photo } from './photo';
+
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    // Optional variables are marked with ? (Elvis operator), always have to come after required var.
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    // Array of photos from Photo interface
+    photos?: Photo[];
+}
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 // angular router operates on a first match wins system. Order of paths important( we can't have wildcard path on top)
 export const appRoutes: Routes = [
@@ -14,9 +17,10 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            {path: 'members', component: MemberListComponent},
+            {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+            {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
             {path: 'messages', component: MessagesComponent},
-            {path: 'lists', component: ListsComponent},
+            {path: 'lists', component: ListsComponent}
         ]
     },
     // wildcard path that redirects to home when the url path doesn't match any of the previous options
